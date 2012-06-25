b1f205bb508cff5ff0de3bf405c850be1f483ceb
Stefan Magnus Landrø <stefan.landro@gmail.com>
2012-06-25 22:40:39 +0200

    Added documentation for one method. To be written for all procs.

--- assert.tcl
+++ assert.tcl
@@ -5,6 +5,19 @@ namespace eval ::testcl {
   namespace export assertNumber
 }
 
+# testcl::assertString --
+#
+# Asserts that actual string value matches expected string value
+#
+# Arguments:
+# expected The expected string value
+# actual The actual string value
+#
+# Side Effects:
+# None.
+#
+# Results:
+# Returns error if values don't match
 proc ::testcl::assertString {expected actual} {
   log::log debug "Expecting '$expected', got actual '$actual'"
   if {$expected ne $actual} {
