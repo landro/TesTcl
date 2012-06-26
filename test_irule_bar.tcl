9f9c1dd3f9a488ca3df786491f8d96b13e859607
Stefan Magnus Landrø <stefan.landro@gmail.com>
2012-06-26 14:07:47 +0200

    moved irule methods to namespace

--- test_irule_bar.tcl
+++ test_irule_bar.tcl
@@ -4,6 +4,9 @@ source onirule.tcl
 namespace import ::testcl::on
 namespace import ::testcl::unknown
 namespace import ::testcl::endstate
+namespace import ::testcl::event
+namespace import ::testcl::when
+namespace import ::testcl::rule
 namespace import ::testcl::assertStringEquals
 namespace import ::testcl::assertNumberEquals
 
