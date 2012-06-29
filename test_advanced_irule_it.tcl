30bf9fe26f0516dadc1d50ce235575d229426895
Stefan Magnus Landrø <stefan.landro@gmail.com>
2012-06-29 16:15:59 +0200

    added more tests

--- test_advanced_irule_it.tcl
+++ test_advanced_irule_it.tcl
@@ -59,3 +59,57 @@ it "should block access to uri /blocked" {
   assertNumberEquals 0 $rc
 
 }
+
+it "should give apache http client a correct error code when app pool is down" {
+
+  event HTTP_REQUEST
+
+  on HTTP::header insert X-Forwarded-SSL true return ""
+  on HTTP::uri return "/app"
+  on active_members pool_application return 0
+  on HTTP::header User-Agent return "Apache HTTP Client"
+
+  endstate HTTP::respond 503
+
+  run advanced_irule.tcl rc result
+
+  assertStringEquals "rule irule" $result
+  assertNumberEquals 0 $rc
+
+}
+
+it "should give other clients then apache http client redirect to fallback when app pool is down" {
+
+  event HTTP_REQUEST
+
+  on HTTP::header insert X-Forwarded-SSL true return ""
+  on HTTP::uri return "/app"
+  on active_members pool_application return 0
+  on HTTP::header User-Agent return "Firefox 13.0.1"
+
+  endstate HTTP::redirect "http://fallback.com"
+
+  run advanced_irule.tcl rc result
+
+  assertStringEquals "rule irule" $result
+  assertNumberEquals 0 $rc
+
+}
+
+it "should give handle app request using app pool when app pool is up" {
+
+  event HTTP_REQUEST
+
+  on HTTP::header insert X-Forwarded-SSL true return ""
+  on HTTP::uri return "/app"
+  on HTTP::uri /app return ""
+  on active_members pool_application return 2
+
+  endstate pool pool_application
+
+  run advanced_irule.tcl rc result
+
+  assertStringEquals "rule irule" $result
+  assertNumberEquals 0 $rc
+
+}
