28e6f4776343a8b957e6a33cd0adbca69979c628
Stefan Magnus Landrø <stefan.landro@gmail.com>
2012-06-29 15:58:19 +0200

    added more tests

--- test_advanced_irule_it.tcl
+++ test_advanced_irule_it.tcl
@@ -5,9 +5,9 @@ source it.tcl
 namespace import ::testcl::*
 
 # Comment out to suppress logging
-log::lvSuppressLE info 0
+#log::lvSuppressLE info 0
 
-it "should handle admin request using pool admin when credentials are ok" {
+it "should handle admin request using pool admin when credentials are valid" {
 
   event HTTP_REQUEST
 
@@ -36,6 +36,22 @@ it "should ask for credentials when admin request without correct credentials" {
   on HTTP::password return "wrong_password"
 
   endstate HTTP::respond 401 WWW-Authenticate "Basic realm=\"Restricted Area\""
+
+  run advanced_irule.tcl rc result
+
+  assertStringEquals "rule irule" $result
+  assertNumberEquals 0 $rc
+
+}
+
+it "should block access to uri /blocked" {
+
+  event HTTP_REQUEST
+
+  on HTTP::header insert X-Forwarded-SSL true return ""
+  on HTTP::uri return "/blocked"
+
+  endstate HTTP::respond 403
 
   run advanced_irule.tcl rc result
 
