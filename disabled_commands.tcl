42e4a58c3c7c220a540b3a5ce881413c3331fd19
Stefan Magnus Landrø <stefan.landro@gmail.com>
2012-10-03 20:58:08 +0200

    added list of disabled commands

---
+++ disabled_commands.tcl
@@ -0,0 +1,55 @@
+# The following web-page documents which Tcl commands are disabled in iRules
+# https://devcentral.f5.com/wiki/iRules.DisabledTclCommands.ashx
+
+# after (enabled in 10.x)
+# auto_execok
+# auto_import
+# auto_load
+# auto_mkindex
+# auto_mkindex_old
+# auto_qualify
+# auto_reset
+# bgerror
+# cd
+# close
+# dict
+# eof
+# encoding
+# exec
+# exit
+# fblocked
+# fconfigure
+# fcopy
+# file
+# fileevent
+# filename
+# flush
+# gets
+# glob
+# http
+# interp
+# load
+# lrepeat
+# lreverse
+# memory
+# namespace
+# open
+# package
+# pid
+# pkg::create
+# pkg_mkIndex
+# proc
+# pwd
+# rename
+# seek
+# socket
+# source
+# tcl_findLibrary
+# tell
+# time
+# unknown
+# update
+# uplevel
+# upvar
+# vwait
+
