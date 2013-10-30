7206c5c98f237cbe7267205327f26c5a07f9a85d
Stefan Magnus Landrø <stefan.landro@gmail.com>
2013-10-30 15:04:59 +0100

    reformated in order to make it easy to copy paste lines into jtcl repl

--- verification/test_jtcl_irule_all.tcl
+++ verification/test_jtcl_irule_all.tcl
@@ -1,19 +1,9 @@
 # Use this script to verify that your jtcl installation has been 
 # patched correctly to support iRule tcl extensions
-if {"aa" starts_with "a"} {
-  puts "starts_with works as expected"
-}
-if {"a" equals "a"} {
-  puts "equals works as expected"
-}
-if { not false } {
-  puts "not works as expected"
-}
-if { 1 and 1} {
-  puts "and works as expected"
-}
-if { 1 or 0} {
-  puts "or works as expected"
-}
+if {"aa" starts_with "a"} { puts "starts_with works as expected" }
+if {"a" equals "a"} { puts "equals works as expected" }
+if { not false } { puts "not works as expected" }
+if { 1 and 1} { puts "and works as expected" }
+if { 1 or 0} { puts "or works as expected" }
 
 
