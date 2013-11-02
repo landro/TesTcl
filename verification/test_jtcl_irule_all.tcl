fd51b36e91cc04a11df01065b89641fc912ffff4
Stefan Magnus Landrø <stefan.landro@gmail.com>
2013-11-02 23:17:37 +0100

    added more verifications for irule jtcl extension

--- verification/test_jtcl_irule_all.tcl
+++ verification/test_jtcl_irule_all.tcl
@@ -1,9 +1,15 @@
 # Use this script to verify that your jtcl installation has been 
 # patched correctly to support iRule tcl extensions
-if {"aa" starts_with "a"} { puts "starts_with works as expected" }
-if {"a" equals "a"} { puts "equals works as expected" }
-if { not false } { puts "not works as expected" }
+if {"abcdefg" starts_with "ab"} { puts "starts_with works as expected" }
+if {"abcdefg" ends_with "efg"} { puts "ends_with works as expected" }
+if {"abcdefg" contains "cde"} { puts "contains works as expected" }
+# TODO Unsure why glob does not match
+if {"abcdefg" matches_glob "abcd*"} { puts "matches_glob works as expected" }
+if {"abcdefg" matches_regex "a.cde.*"} { puts "matches_regex works as expected" }
+
+if {"abcdefg" equals "abcdefg"} { puts "equals works as expected" }
 if { 1 and 1} { puts "and works as expected" }
 if { 1 or 0} { puts "or works as expected" }
+if { not false } { puts "not works as expected" }
 
 
