11307f562f09f59acbacb7d460575e24a949a604
Stefan Magnus Landrø <stefan.landro@gmail.com>
2013-11-08 12:03:01 +0100

    added parameter in order to support running tests using tclsh

--- tests.sh
+++ tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 for f in test/test_*.tcl
 do
-	jtcl "$f"
+    if [ 'tclsh' == "$1" ] ; then tclsh "$f";
+    elif [ 'jtcl' == "$1" ] ; then jtcl "$f";
+    else echo "Usage: ./tests.sh [jtcl|tclsh]"; exit 1;
+    fi
 done
