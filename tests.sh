a88c2d215bd75d812693a2d86b3d1dd96b1a100c
Neil Hemingway <neil.hemingway@netbanx.com>
2015-05-19 17:00:58 +0100

    Add a rough and ready indication of which tests failed

--- tests.sh
+++ tests.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
-for f in test/test_*.tcl
-do
-    if [ 'tclsh' == "$1" ] ; then tclsh "$f";
-    elif [ 'jtcl' == "$1" ] ; then jtcl "$f";
+
+function run_test() {
+    if [ 'tclsh' == "$1" ] ; then tclsh "$2";
+    elif [ 'jtcl' == "$1" ] ; then jtcl "$2";
     else echo "Usage: ./tests.sh [jtcl|tclsh]"; exit 1;
     fi
+}
+
+failures=()
+
+for file in test/test_*.tcl
+do
+    run_test "$1" "$file"
+    if [ $? -gt 0 ] ; then
+        failures+=($file)
+    fi
 done
+
+echo "Test Summary"
+echo "============"
+if [ ${#failures[@]} -gt 0 ] ; then
+    echo ${#failures[@]} " tests failed:"
+    for failure in ${failures[@]} ; do
+        echo "    ${failure}"
+    done
+    exit ${#failures[@]}
+else
+    echo "All tests successful"
+    exit 0
+fi
