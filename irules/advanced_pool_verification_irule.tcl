df2c0aca4e4375775bc22c3727806de21cb68ada
Guillemo Szeliga <gszeliga@newrelic.com>
2018-11-16 13:08:00 +0100

    Add usage example

---
+++ irules/advanced_pool_verification_irule.tcl
@@ -0,0 +1,19 @@
+rule advances_pool {
+
+  when HTTP_REQUEST priority 100 {
+      
+      set new_pool ""
+      set hostname [string tolower [getfield [HTTP::host] ":" 1]]      
+
+      if { not [ catch { set next_step [class match -value -- $hostname equals "foo-datagroup"] } ] } {
+          if { $next_step ne "" } {
+              set new_pool $next_step
+          }
+      }
+
+      if { [catch { pool $new_pool } ] } {
+          log local0. "Failed to find pool '$new_pool', sending to '[LB::server pool]'"
+          pool [LB::server pool]
+      }      
+  }
+}
